f52fcbf35154191c067c93c0801f771f36413386
Shreyagundu <gundus@iitbhilai.ac.in>
2019-10-13 03:49:12 +0530

    read_index_added

--- gitroot.py
+++ gitroot.py
@@ -4,6 +4,22 @@ import argparse, collections, difflib, enum, hashlib, operator, os, stat
 import struct, sys, time, urllib.request, zlib
 
 
+# Data for one entry in the git index (.git/index)
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode', 'uid',
+    'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+
+class ObjectType(enum.Enum):
+    """Object type enum. There are other types too, but we don't need them.
+    See "enum object_type" in git's source (git/cache.h).
+    """
+    commit = 1
+    tree = 2
+    blob = 3
+
+
 def init(repo):
     """Create directory for repo and initialize .git directory."""
     os.mkdir(repo)
@@ -97,6 +113,32 @@ def cat_file(mode, sha1_prefix):
         raise ValueError('unexpected mode {!r}'.format(mode))
 
 
+def read_index():
+    """Read git index file and return list of IndexEntry objects."""
+    try:
+        data = read_file(os.path.join('.git', 'index'))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], 'invalid index checksum'
+    signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+    assert signature == b'DIRC', \
+            'invalid index signature {}'.format(signature)
+    assert version == 2, 'unknown index version {}'.format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH', entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = IndexEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+    assert len(entries) == num_entries
+    return entries
 
 
 if __name__ == '__main__':
