cfc831c71beebe679e3f8bdb6939c2357952d4bb
KShivendu <shivendu@iitbhilai.ac.in>
2019-10-13 12:51:15 +0530

    Added hash_object function

--- gitroot.py
+++ gitroot.py
@@ -20,6 +20,21 @@ def write_file(path, data):
     with open(path, 'wb') as f:
         f.write(data)
 
+def hash_object(data, obj_type, write=True):
+    """Compute hash of object data of given type and write to object store if
+    "write" is True. Return SHA-1 object hash as hex string.
+    """
+    header = '{} {}'.format(obj_type, len(data)).encode()
+    full_data = header + b'\x00' + data
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            write_file(path, zlib.compress(full_data))
+    return sha1
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     sub_parsers = parser.add_subparsers(dest='command', metavar='command')
