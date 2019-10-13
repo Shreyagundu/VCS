d53121abb9dea5d1c88526947d7cf26f3d3f27d1
KShivendu <shivendu@iitbhilai.ac.in>
2019-10-13 22:25:19 +0530

    Added function for adjusting error in add

--- gitroot.py
+++ gitroot.py
@@ -71,6 +71,24 @@ def read_object(sha1_prefix):
             size, len(data))
     return (obj_type, data)
 
+def add(paths):
+    """Add all file paths to git index."""
+    paths = [p.replace('\\', '/') for p in paths]
+    all_entries = read_index()
+    entries = [e for e in all_entries if e.path not in paths]
+    for path in paths:
+        sha1 = hash_object(read_file(path), 'blob')
+        st = os.stat(path)
+        flags = len(path.encode())
+        assert flags < (1 << 12)
+        entry = IndexEntry(
+                int(st.st_ctime), 0, int(st.st_mtime), 0, st.st_dev,
+                st.st_ino, st.st_mode, st.st_uid, st.st_gid, st.st_size,
+                bytes.fromhex(sha1), flags, path)
+        entries.append(entry)
+    entries.sort(key=operator.attrgetter('path'))
+    write_index(entries)
+
 
 def cat_file(mode, sha1_prefix):
     """Write the contents of (or info about) object with given SHA-1 prefix to
